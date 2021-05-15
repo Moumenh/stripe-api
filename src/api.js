aa0ea51801ec4cbd38f718ed8c178c90e3ae18d7
Momen Hanbali <momen.hanbali@xngage.com>
2021-05-15 17:33:54 +0300

    err handling

--- src/api.js
+++ src/api.js
@@ -1,46 +1,62 @@
-const express = require("express");
-const serverless = require("serverless-http");
-const stripe = require('stripe')(process.env.STRIPE_SECRET);
+const express = require("express")
+const serverless = require("serverless-http")
+const stripe = require('stripe')(process.env.STRIPE_SECRET)
 
-const app = express();
-const router = express.Router();
+const app = express()
+const router = express.Router()
 
 router.get('/plans', async (req, res) => {
-  const plans = await stripe.plans.list({ product: 'prod_JTzpVJTJZkekNe' });
-  res.json(plans)
+  try {
+    const plans = await stripe.plans.list({ product: 'prod_JTzpVJTJZkekNe' })
+    res.json(plans)
+  } catch (err) {
+    res.json({ error: true, error: err.message })
+  }
 })
-
 
 router.post('/pay', async (req, res) => {
   console.log(req.body)
-  const { id } = await stripe.customers.create({
-    email: req.body.email,
-    source: req.body.token,
-  });
-  const subscription = await stripe.subscriptions.create({
-    customer: id,
-    items: [
-      { price: req.body.plan },
-    ],
-  });
-  res.json(subscription)
-})
+  try {
+    const { id } = await stripe.customers.create({
+      email: req.body.email,
+      source: req.body.token,
+    })
+    const subscription = await stripe.subscriptions.create({
+      customer: id,
+      items: [
+        { price: req.body.plan },
+      ],
+    })
+    res.json(subscription)
+  } catch (err) {
+    res.json({ error: true, error: err.message })
+  }
 
+})
 
 router.post('/subscription', async (req, res) => {
-  const customer = await stripe.customers.retrieve(
-    req.body.customerId
-  );
-  res.json(customer)
+  try {
+    const customer = await stripe.customers.retrieve(
+      req.body.customerId
+    )
+    res.json(customer)
+  } catch (err) {
+    res.json({ error: true, error: err.message })
+  }
 })
+
 router.post('/unsubscribe', async (req, res) => {
-  const deleted = await stripe.subscriptions.retrieve(
-    req.body.subId
-  );
-  res.json(deleted)
+  try {
+    const deleted = await stripe.subscriptions.retrieve(
+      req.body.subId
+    )
+    res.json(deleted)
+  } catch (err) {
+    res.json({ error: true, error: err.message })
+  }
 })
 
-app.use(`/.netlify/functions/api`, router);
+app.use(`/.netlify/functions/api`, router)
 
-module.exports = app;
-module.exports.handler = serverless(app);
+module.exports = app
+module.exports.handler = serverless(app)
